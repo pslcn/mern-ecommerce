537bc7f8ac410cee49c0847abd5bd44465da30d2
pslcn <gregorsrussellcobb@gmail.com>
2023-01-14 17:28:17 +0000

    Limit number of chars in desc

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import {useState} from 'react';
 
 const Product = (props) => {
 	const [quantity, setQuantity] = useState(0);
+	const descmaxlen = 80;
 	return (
 		<div className="Product">
 			<h2>{props.name}</h2>
-			<p>{props.desc}</p>
+			<p>{props.desc.length > descmaxlen ? 
+				props.desc.substring(0, descmaxlen - 3) + '...' : 
+				props.desc
+			}</p>
 			<p>{props.price}</p>
 			<button onClick={() => {alert('Item added to cart'); setQuantity((prevCount) => prevCount + 1)}}>Add to cart</button>
 			<p>Quantity: {quantity}</p>
