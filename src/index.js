f890aad4f0a40850e0a7f5850a328f2b6a9d1aaa
pslcn <gregorsrussellcobb@gmail.com>
2023-01-17 09:41:13 +0000

    Start adding MongoDB interaction for search bar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { config } from 'dotenv';
 
 import App from './App';
+
+config();
+console.log('MongoDB on localhost: ' + process.env.DB_URI);
 
 createRoot(document.getElementById('root')).render(
 	<App />
